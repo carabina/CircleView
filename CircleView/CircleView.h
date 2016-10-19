fbad667164bc5e386becc17553ff8b2e447f7310
Programming&Occults <komokurono@gmail.com>
2016-10-19 11:10:14 +0200

    Initial Commit

---
+++ CircleView/CircleView.h
@@ -0,0 +1,19 @@
+//
+//  CircleView.h
+//  CircleView
+//
+//  Created by Diana Komolova on 19.10.2016.
+//  Copyright © 2016 Programming&Occults. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CircleView.
+FOUNDATION_EXPORT double CircleViewVersionNumber;
+
+//! Project version string for CircleView.
+FOUNDATION_EXPORT const unsigned char CircleViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CircleView/PublicHeader.h>
+
+
